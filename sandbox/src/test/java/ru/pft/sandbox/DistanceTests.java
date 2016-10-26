df6d3210de5036a314c3d2d643139ed00f9977f2
dmitry-titov <titov.dmitri9@gmail.com>
2016-10-26 23:49:58 +0300

    Добавлен тест проверки расчета расстояния для одинаковых точек, исправлены имена тестов

--- sandbox/src/test/java/ru/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/pft/sandbox/DistanceTests.java
@@ -9,23 +9,30 @@ import org.testng.annotations.Test;
 public class DistanceTests {
 
   @Test
-  public void checkDistanceAssertEquals(){
+  public void checkDistanceAssertEqualsTest(){
     Point p1 = new Point(1,2);
     Point p2 = new Point(2,5);
     Assert.assertEquals(Math.round(p1.distance(p2)), 3);
   }
 
   @Test
-  public void checkDistanceAssertFalse(){
+  public void checkDistanceAssertFalseTest(){
     Point p1 = new Point(1,2);
     Point p2 = new Point(2,8);
     Assert.assertFalse(Math.round(p1.distance(p2))!= 6);
   }
 
   @Test
-  public void checkDistanceAssertTrue(){
+  public void checkDistanceAssertTrueTest(){
     Point p1 = new Point(1,7);
     Point p2 = new Point(2,2);
     Assert.assertTrue(p1.distance(p2) != 2);
+  }
+
+  @Test
+  public void checkDistanceSamePointTest(){
+    Point p1 = new Point(7,7);
+    Point p2 = new Point(7,7);
+    Assert.assertEquals(p1.distance(p2), 0.0);
   }
 }
