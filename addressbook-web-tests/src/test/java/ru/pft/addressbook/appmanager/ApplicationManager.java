6807288dffee22443497a153fa0608268177a492
dmitry-titov <titov.dmitri9@gmail.com>
2016-12-18 23:58:50 +0300

    Исправлен адрес selenium server

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.pft.addressbook.appmanager;
 
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
